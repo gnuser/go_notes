3153e20970b762d342fc86cd0a638eab955314a9
gnuser <cjay0106@gmail.com>
2018-07-22 22:27:30 +0800

    Add function definition

--- function/6_1_definition.go
+++ function/6_1_definition.go
@@ -23,6 +23,12 @@ func (n *N) pointer() {
 	fmt.Printf("p: %p, %v\n", n, *n)
 }
 
+type X struct {}
+
+func (x *X) test() {
+	println("hi!", x)
+}
+
 func main() {
 	var a N = 25
 	fmt.Printf("origin addr: %p, %#x\n", &a, a)
@@ -32,4 +38,23 @@ func main() {
 	fmt.Println("2", a.toString())
 	a.pointer()
 	fmt.Println("3", a.toString())
+	fmt.Println("===========")
+	// 自动使用对应类型
+	p := &a
+	p.value()
+	fmt.Println("4", a.toString())
+	p.pointer()
+	fmt.Println("5", a.toString())
+
+	// 不能用多级指针
+	//p2 := &p
+	//p2.value() // calling method value with receiver p2 (type **N) requires explicit dereference
+	//p2.pointer() // calling method pointer with receiver p2 (type **N) requires explicit dereference
+
+	fmt.Println("===========")
+	var b *X
+	b.test()     // 还没有初始化的指针,也可以调用
+	//X{}.test() // 不是指针不行
+	(&X{}).test()
+
 }
